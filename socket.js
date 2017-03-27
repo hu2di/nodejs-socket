9a0c4e6f5dc66e2abf2efb703dd9709a8070cb08
HuyHung Dinh <hebitaxy@gmail.com>
2017-03-27 11:22:03 +0700

    First commit

---
+++ socket.js
@@ -0,0 +1,19 @@
+var socketIO = require('socket.io'),
+	http = require('http'),
+	port = process.env.PORT || 8080,
+	ip = process.env.IP || '127.0.0.1',
+	server = http.createServer().listen(port, ip, function() {
+		console.log('Started Socket.IO');
+	}),
+	io = socketIO.listen(server);
+io.set('match origin procotol', true);
+io.set('origin', '*:*');
+var run = function (socket) {
+	socket.emit('access', 'Access successfully:');
+	socket.on('user-join', function (data) {
+		console.log('User ' + data + ' have joined');
+		socket.broadcast.emit('new-user ', data);
+		//Emit gui, on la cho de nhan va xu li
+	});
+}
+io.sockets.on('connection', run);
